fd8c64b90e596baec77f99c837f81208e1162523
Leonardo Madrid <lemadridb@gmail.com>
2025-11-24 15:13:19 -0600

    Nov.24.2025

--- tripmine/core/core.c
+++ tripmine/core/core.c
@@ -1,80 +1,211 @@
-#define _GNU_SOURCE
-
-#include <sched.h>      // clone(), unshare(), CLONE_* flags
-#include <sys/mount.h>  // mount(), MS_* flags
-#include <sys/wait.h>   // waitpid()
-#include <sys/syscall.h>// syscall() for pivot_root
-#include <unistd.h>     // execve(), sethostname()
-#include <sys/stat.h>   // mkdir()
-#include <fcntl.h>      // open(), O_WRONLY
-#include <stdio.h>      // printf(), perror()
-#include <stdlib.h>     // malloc(), free(), exit()
-#include <string.h>     // strerror()
-#include <errno.h>      // errno
-
-#define STACK_SIZE (1024 * 1024)
+#define _GNU_SOURCE // Required for unshare, pivot_root definitions
+#include <sched.h>   // For unshare()
+#include <sys/mount.h> // For mount()
+#include <unistd.h>  // For fork(), exec*, chdir(), syscall()
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/syscall.h> // For syscall(SYS_pivot_root, ...)
 
 struct CubeConfig {
     int in_pipe;
     int out_pipe;
-    char **command_list;
-    char *hostname;
-    char *cwd;
-    char *env[];
+    int cube_id;
+    char* image_name;
+    char** commands;
 };
 
-// Inside the Cube Namespace
-static int child_function(void *arg) {
-    struct CubeConfig *config = (struct CubeConfig *)arg;
+static int child_function(void *args) {
+    struct CubeConfig *config = (struct CubeConfig *)args;
 
-    // TODO: Execution
+    // Pipe
+    dup2(config->in_pipe, STDIN_FILENO);
+    dup2(config->out_pipe, STDOUT_FILENO);
+    dup2(config->out_pipe, STDERR_FILENO);
     
+    close(config->in_pipe);
+    close(config->out_pipe);
 
-    return 1;
-}
+    setup_fs(config);
+    enter_jail(config->cube_id);
 
-void parse_args(int argc, char *argv[] struct CubeConfig *cfg) {
-    // Defaults
-    cfg->env = [
-        "TRIP_PATH=/",
-        "HOME=/",
-        "TERM=xterm-256color",
+    char* env[] = {
+        "TRIP_PATH=/bin",
         "PROMPT=false",
         NULL
-    ];
-    cfg->hostname = "tripmine";
-    cfg->cwd = "/tripmine/";
+    };
+
+    execve("/bin/sh", NULL, env);
     
-    // TODO: Parsing
-    for (int i = 1; i < argc; i++) {
-        if (strcmp(argv[i], "--hostname") == 0) {
-            cfg->hostname = argv[i + 1];
-            cfg->cwd = malloc(strlen("/" + strlen(cfg->hostname) + strlen("/"));
-            cfg->cwd = strcat("/", cfg->hostname, "/");
-        } else if (strcmp(argv[i], "--env") == 0) {
-            cfg->env = argv[i + 1];
-        } else if (strcmp(argv[i], "--command") == 0) {
-            cfg->command_list = argv[i + 1];
+    return 0;
+}
+
+// Recursive directory function
+void mkdir_p(const char *path) {
+    char tmp[256];
+    char *p = NULL;
+    size_t len;
+
+    snprintf(tmp, sizeof(tmp), "%s", path);
+    len = strlen(tmp);
+    if (tmp[len - 1] == '/') tmp[len - 1] = 0;
+
+    for (p = tmp + 1; *p; p++) {
+        if (*p == '/') {
+            *p = 0;
+            mkdir(tmp, 0755); // Ignore error if exists
+            *p = '/';
         }
     }
+    mkdir(tmp, 0755);
+}
+
+// Define the pivot_root system call wrapper since glibc doesn't have one
+int pivot_root(const char *new_root, const char *put_old) {
+    return syscall(SYS_pivot_root, new_root, put_old);
+}
+
+// Enter the jail
+void enter_jail(int cube_id) {
+    char new_root[256];
+    char old_root[256];
+    
+    sprintf(new_root, "/var/tripmine/cubes/%d/", cube_id);
+    sprintf(old_root, "%s/old_root", new_root);
+
+    mkdir(old_root, 0755);
+    
+    if (mount(new_root, new_root, NULL, MS_BIND | MS_REC, NULL) != 0) {
+        perror("Self bind-mount failed");
+        exit(1);
+    }
+    
+    printf("[Cube %d] Pivotting root...\n", cube_id);
+    if (pivot_root(new_root, old_root) != 0) {
+        perror("Pivot failed");
+        exit(1);
+    }
+
+    chdir("/");
+    printf("[Cube %d] Pivot done.\n", cube_id);
+    
+    if (umount2("/old_root", MNT_DETACH) != 0) {
+        perror("Old root unmount failed");
+        exit(1);
+    }
+
+    rmdir("/old_root");
+
+    printf("[Cube %d] Locked inside. Ready to proceed.\n", cube_id);
+}
+
+// Setup the filesystem for the cube.
+void setup_fs(struct CubeConfig *config) {
+    // Important 
+    char host_mirrors[256], host_frame[256];
+    char cube_root[256], target_bin[256], target_lib[256];
+    char* shell_path = "/var/tripmine/bin/shell";
+    char target_shell[256];
+
+    /*
+        TODO:
+        1. Parse args
+        2. Create a new mount namespace *
+        3. Mount *
+        4. Load the dependencies into /bin/ of the namespace *
+        5. Pivot the root to the new root *
+        6. Execute the command
+    */
+
+    int cube_id = config->cube_id;
+    char* image_name = config->image_name;
+
+    // Prepare for the new namespace
+    sprintf(host_mirrors, "/var/tripmine/images/%s/mirrors/", image_name);
+    sprintf(host_frame, "/var/tripmine/images/%s/frames/", image_name);
+    sprintf(cube_root, "/var/tripmine/cubes/%d/", cube_id);
+    sprintf(target_bin, "%s/bin/", cube_root);
+    sprintf(target_lib, "%s/lib/", cube_root);
+    sprintf(target_shell, "%s/bin/sh", cube_root);
+    
+    mkdir_p(cube_root);
+    mkdir_p(target_bin);
+    mkdir_p(target_lib);
+
+    // Mount Mirrors->/bin/ (READ ONLY)
+    if (mount(host_mirrors, target_bin, NULL, MS_BIND | MS_REC, NULL) != 0) {
+        perror("Mirror mounting failed");
+        exit(1);
+    }
+    mount(NULL, target_bin, NULL, MS_BIND | MS_REMOUNT | MS_RDONLY, NULL);
+
+    // Mount Frame->/lib/ (READ WRITE)
+    if (mount(host_frame, target_lib, NULL, MS_BIND | MS_REC, NULL) != 0) {
+        perror("Frame mounting failed");
+        exit(1);
+    }
+
+    // Mount shell into /bin/sh
+    int fd = open(target_shell, O_CREAT, 0755);
+    if (fd >= 0) {
+        close(fd);
+    }
+
+    if (mount(shell_path, target_shell, NULL, MS_BIND, NULL) != 0) {
+        perror("Shell mounting failed");
+        exit(1);
+    }
+    mount(NULL, target_shell, NULL, MS_BIND | MS_REMOUNT | MS_RDONLY, NULL);
+
+    printf("[Cube %d] FS Steup done.\n", cube_id);
+    printf("  %s -> /bin (RO)\n", host_mirrors);
+    printf("  %s -> /lib (RW)\n", host_frame);
+    printf("[Cube %d] Cube is ready to proceed.\n", cube_id);
+    return 0;
 }
 
 int main(int argc, char *argv[]) {
-    // Define and structure
     struct CubeConfig config;
-    parse_args(argc, argv, &config);
-    
-    char *stack = malloc(STACK_SIZE);
-    pid_t pid = clone(child_function,
-        stack + STACK_SIZE,
-        CLONE_NEWUTS | CLONE_NEWPID | CLONE_NEWNS | CLONE_NEWNET | SIGCHLD,
-        &config
-    );
+    config.cube_id = argv[1];
+    config.image_name = argv[2];
+    for (int i = 3; i < argc; i++) {
+        config.commands[i - 3] = argv[i];
+    }
+    config.commands[argc - 3] = NULL;
 
-    if (pid == -1) { perror("Clone Failed"); exit(1); }
+    int pipein[2];
+    int pipeout[2];
+    if (pipe(pipein) == -1 || pipe(pipeout) == -1) {
+        perror("pipe");
+        exit(1);
+    }
 
-    close(log_pipe[1]);
+    config.in_pipe = pipein[0];
+    config.out_pipe = pipeout[1];
+
+    pid_t pid = fork();
+    if (pid == -1) {
+        perror("fork");
+        exit(1);
+    } else if (pid == 0) {
+        close(pipein[0]);
+        close(pipeout[1]);
+        child_function(&config);
+    } else {
+        close(pipein[1]);
+        close(pipeout[0]);
+    }
+
+    char buffer[1024];
+    int n;
+    while ((n = read(pipeout[0], buffer, sizeof(buffer))) > 0) {
+        write(STDOUT_FILENO, buffer, n);
+    }
+    close(pipeout[0]); 
+
     waitpid(pid, NULL, 0);
+    close(pipein[0]);
+    close(pipeout[1]);
 
     return 0;
 }
